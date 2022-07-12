c4f300af4bc9719f2099b1e3dc51d466cccafd15
Yama-Can <yama.can95@gmail.com>
2022-07-12 18:03:38 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const { Client, Intents } = require('discord.js');
+var http = require('http')
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS] });
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    response.end('<h1><span id="Hello_World">Hello World</span></h1>');
+})
+
+server.listen(8080)
+
+const { Client, Intents } = require('discord.js')
+
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS],
+})
 
 client.once('ready', () => {
-    console.log('Ready!'); // 起動した時に"Ready!"とコンソールに出力する
-});
+    console.log('Ready!') // 起動した時に"Ready!"とコンソールに出力する
+})
 
-client.on("guildMemberAdd", member => {
-    if (member.guild.id !== "利用するサーバーId") return; // 指定のサーバー以外では動作しないようにする
-    member.guild.channels.cache.get("送信するチャンネルId").send(`${member.user}が参加しました！`);
-});
+client.on('guildMemberAdd', (member) => {
+    if (member.guild.id !== '利用するサーバーId') return // 指定のサーバー以外では動作しないようにする
+    member.guild.channels.cache
+        .get('送信するチャンネルId')
+        .send(`${member.user}が参加しました！`)
+})
 
-client.login("先ほどメモしたToken");
+client.login('先ほどメモしたToken')
